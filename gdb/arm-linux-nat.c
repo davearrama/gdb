c9fbab354f2547744418995d572dab8bf37cda00
scottb <scottb>
2000-04-10 21:07:38 +0000

    Move arm_skip_solib_resolver from arm-linux-nat.c to arm-linux-tdep.c.
    2000-04-10  Philip Blundell <philb@gnu.org>
    
     	* arm-linux-nat.c (arm_skip_solib_resolver): Remove and move to
     	arm-linux-tdep.c.
     	* arm-linux-tdep.c (arm_skip_solib_resolver): New.

--- gdb/arm-linux-nat.c
+++ gdb/arm-linux-nat.c
@@ -1,5 +1,5 @@
 /* GNU/Linux on ARM native support.
-   Copyright 1999 Free Software Foundation, Inc.
+   Copyright 1999, 2000 Free Software Foundation, Inc.
 
    This file is part of GDB.
 
@@ -72,7 +72,7 @@ FPA11;
 static unsigned int os_version, os_major, os_minor, os_release;
 
 static void
-fetch_nw_fpe_single (unsigned int fn, FPA11 * fpa11, unsigned int *pmem)
+fetch_nwfpe_single (unsigned int fn, FPA11 * fpa11)
 {
   unsigned int mem[3];
 
@@ -83,7 +83,7 @@ fetch_nw_fpe_single (unsigned int fn, FPA11 * fpa11, unsigned int *pmem)
 }
 
 static void
-fetch_nw_fpe_double (unsigned int fn, FPA11 * fpa11, unsigned int *pmem)
+fetch_nwfpe_double (unsigned int fn, FPA11 * fpa11)
 {
   unsigned int mem[3];
 
@@ -94,7 +94,7 @@ fetch_nw_fpe_double (unsigned int fn, FPA11 * fpa11, unsigned int *pmem)
 }
 
 static void
-fetch_nw_fpe_none (unsigned int fn, FPA11 * fpa11, unsigned int *pmem)
+fetch_nwfpe_none (unsigned int fn)
 {
   unsigned int mem[3] =
   {0, 0, 0};
@@ -103,7 +103,7 @@ fetch_nw_fpe_none (unsigned int fn, FPA11 * fpa11, unsigned int *pmem)
 }
 
 static void
-fetch_nw_fpe_extended (unsigned int fn, FPA11 * fpa11, unsigned int *pmem)
+fetch_nwfpe_extended (unsigned int fn, FPA11 * fpa11)
 {
   unsigned int mem[3];
 
@@ -114,7 +114,7 @@ fetch_nw_fpe_extended (unsigned int fn, FPA11 * fpa11, unsigned int *pmem)
 }
 
 static void
-store_nw_fpe_single (unsigned int fn, FPA11 * fpa11)
+store_nwfpe_single (unsigned int fn, FPA11 * fpa11)
 {
   unsigned int mem[3];
 
@@ -124,7 +124,7 @@ store_nw_fpe_single (unsigned int fn, FPA11 * fpa11)
 }
 
 static void
-store_nw_fpe_double (unsigned int fn, FPA11 * fpa11)
+store_nwfpe_double (unsigned int fn, FPA11 * fpa11)
 {
   unsigned int mem[3];
 
@@ -135,7 +135,7 @@ store_nw_fpe_double (unsigned int fn, FPA11 * fpa11)
 }
 
 void
-store_nw_fpe_extended (unsigned int fn, FPA11 * fpa11)
+store_nwfpe_extended (unsigned int fn, FPA11 * fpa11)
 {
   unsigned int mem[3];
 
@@ -170,24 +170,23 @@ fetch_fpregs (void)
   for (regno = F0_REGNUM; regno <= F7_REGNUM; regno++)
     {
       int fn = regno - F0_REGNUM;
-      unsigned int *p = (unsigned int *) &registers[REGISTER_BYTE (regno)];
 
       switch (fp.fType[fn])
 	{
 	case typeSingle:
-	  fetch_nw_fpe_single (fn, &fp, p);
+	  fetch_nwfpe_single (fn, &fp);
 	  break;
 
 	case typeDouble:
-	  fetch_nw_fpe_double (fn, &fp, p);
+	  fetch_nwfpe_double (fn, &fp);
 	  break;
 
 	case typeExtended:
-	  fetch_nw_fpe_extended (fn, &fp, p);
+	  fetch_nwfpe_extended (fn, &fp);
 	  break;
 
 	default:
-	  fetch_nw_fpe_none (fn, &fp, p);
+	  fetch_nwfpe_none (fn);
 	}
     }
 }
@@ -199,7 +198,6 @@ static void
 store_fpregs (void)
 {
   int ret, regno;
-  unsigned int mem[3];
   FPA11 fp;
 
   /* Store fpsr.  */
@@ -215,15 +213,15 @@ store_fpregs (void)
 	  switch (fp.fType[fn])
 	    {
 	    case typeSingle:
-	      store_nw_fpe_single (fn, &fp);
+	      store_nwfpe_single (fn, &fp);
 	      break;
 
 	    case typeDouble:
-	      store_nw_fpe_double (fn, &fp);
+	      store_nwfpe_double (fn, &fp);
 	      break;
 
 	    case typeExtended:
-	      store_nw_fpe_extended (fn, &fp);
+	      store_nwfpe_extended (fn, &fp);
 	      break;
 	    }
 	}
@@ -324,166 +322,6 @@ store_inferior_registers (int regno)
     store_fpregs ();
 }
 
-#ifdef GET_LONGJMP_TARGET
-
-/* Figure out where the longjmp will land.  We expect that we have
-   just entered longjmp and haven't yet altered r0, r1, so the
-   arguments are still in the registers.  (A1_REGNUM) points at the
-   jmp_buf structure from which we extract the pc (JB_PC) that we will
-   land at.  The pc is copied into ADDR.  This routine returns true on
-   success. */
-
-#define LONGJMP_TARGET_SIZE 	sizeof(int)
-#define JB_ELEMENT_SIZE		sizeof(int)
-#define JB_SL			18
-#define JB_FP			19
-#define JB_SP			20
-#define JB_PC			21
-
-int
-arm_get_longjmp_target (CORE_ADDR * pc)
-{
-  CORE_ADDR jb_addr;
-  char buf[LONGJMP_TARGET_SIZE];
-
-  jb_addr = read_register (A1_REGNUM);
-
-  if (target_read_memory (jb_addr + JB_PC * JB_ELEMENT_SIZE, buf,
-			  LONGJMP_TARGET_SIZE))
-    return 0;
-
-  *pc = extract_address (buf, LONGJMP_TARGET_SIZE);
-  return 1;
-}
-
-#endif /* GET_LONGJMP_TARGET */
-
-/*
-   Dynamic Linking on ARM Linux
-   ----------------------------
-
-   Note: PLT = procedure linkage table
-   GOT = global offset table
-
-   As much as possible, ELF dynamic linking defers the resolution of
-   jump/call addresses until the last minute. The technique used is
-   inspired by the i386 ELF design, and is based on the following
-   constraints.
-
-   1) The calling technique should not force a change in the assembly
-   code produced for apps; it MAY cause changes in the way assembly
-   code is produced for position independent code (i.e. shared
-   libraries).
-
-   2) The technique must be such that all executable areas must not be
-   modified; and any modified areas must not be executed.
-
-   To do this, there are three steps involved in a typical jump:
-
-   1) in the code
-   2) through the PLT
-   3) using a pointer from the GOT
-
-   When the executable or library is first loaded, each GOT entry is
-   initialized to point to the code which implements dynamic name
-   resolution and code finding.  This is normally a function in the
-   program interpreter (on ARM Linux this is usually ld-linux.so.2,
-   but it does not have to be).  On the first invocation, the function
-   is located and the GOT entry is replaced with the real function
-   address.  Subsequent calls go through steps 1, 2 and 3 and end up
-   calling the real code.
-
-   1) In the code: 
-
-   b    function_call
-   bl   function_call
-
-   This is typical ARM code using the 26 bit relative branch or branch
-   and link instructions.  The target of the instruction
-   (function_call is usually the address of the function to be called.
-   In position independent code, the target of the instruction is
-   actually an entry in the PLT when calling functions in a shared
-   library.  Note that this call is identical to a normal function
-   call, only the target differs.
-
-   2) In the PLT:
-
-   The PLT is a synthetic area, created by the linker. It exists in
-   both executables and libraries. It is an array of stubs, one per
-   imported function call. It looks like this:
-
-   PLT[0]:
-   str     lr, [sp, #-4]!       @push the return address (lr)
-   ldr     lr, [pc, #16]   @load from 6 words ahead
-   add     lr, pc, lr      @form an address for GOT[0]
-   ldr     pc, [lr, #8]!   @jump to the contents of that addr
-
-   The return address (lr) is pushed on the stack and used for
-   calculations.  The load on the second line loads the lr with
-   &GOT[3] - . - 20.  The addition on the third leaves:
-
-   lr = (&GOT[3] - . - 20) + (. + 8)
-   lr = (&GOT[3] - 12)
-   lr = &GOT[0]
-
-   On the fourth line, the pc and lr are both updated, so that:
-
-   pc = GOT[2]
-   lr = &GOT[0] + 8
-   = &GOT[2]
-
-   NOTE: PLT[0] borrows an offset .word from PLT[1]. This is a little
-   "tight", but allows us to keep all the PLT entries the same size.
-
-   PLT[n+1]:
-   ldr     ip, [pc, #4]    @load offset from gotoff
-   add     ip, pc, ip      @add the offset to the pc
-   ldr     pc, [ip]        @jump to that address
-   gotoff: .word   GOT[n+3] - .
-
-   The load on the first line, gets an offset from the fourth word of
-   the PLT entry.  The add on the second line makes ip = &GOT[n+3],
-   which contains either a pointer to PLT[0] (the fixup trampoline) or
-   a pointer to the actual code.
-
-   3) In the GOT:
-
-   The GOT contains helper pointers for both code (PLT) fixups and
-   data fixups.  The first 3 entries of the GOT are special. The next
-   M entries (where M is the number of entries in the PLT) belong to
-   the PLT fixups. The next D (all remaining) entries belong to
-   various data fixups. The actual size of the GOT is 3 + M + D.
-
-   The GOT is also a synthetic area, created by the linker. It exists
-   in both executables and libraries.  When the GOT is first
-   initialized , all the GOT entries relating to PLT fixups are
-   pointing to code back at PLT[0].
-
-   The special entries in the GOT are:
-
-   GOT[0] = linked list pointer used by the dynamic loader
-   GOT[1] = pointer to the reloc table for this module
-   GOT[2] = pointer to the fixup/resolver code
-
-   The first invocation of function call comes through and uses the
-   fixup/resolver code.  On the entry to the fixup/resolver code:
-
-   ip = &GOT[n+3]
-   lr = &GOT[2]
-   stack[0] = return address (lr) of the function call
-   [r0, r1, r2, r3] are still the arguments to the function call
-
-   This is enough information for the fixup/resolver code to work
-   with.  Before the fixup/resolver code returns, it actually calls
-   the requested function and repairs &GOT[n+3].  */
-
-CORE_ADDR
-arm_skip_solib_resolver (CORE_ADDR pc)
-{
-  /* FIXME */
-  return 0;
-}
-
 int
 arm_linux_register_u_addr (int blockend, int regnum)
 {
@@ -494,25 +332,6 @@ int
 arm_linux_kernel_u_size (void)
 {
   return (sizeof (struct user));
-}
-
-/* Extract from an array REGBUF containing the (raw) register state
-   a function return value of type TYPE, and copy that, in virtual format,
-   into VALBUF.  */
-
-void
-arm_linux_extract_return_value (struct type *type,
-				char regbuf[REGISTER_BYTES],
-				char *valbuf)
-{
-  /* ScottB: This needs to be looked at to handle the different
-     floating point emulators on ARM Linux.  Right now the code
-     assumes that fetch inferior registers does the right thing for
-     GDB.  I suspect this won't handle NWFPE registers correctly, nor
-     will the default ARM version (arm_extract_return_value()).  */
-
-  int regnum = (TYPE_CODE_FLT == TYPE_CODE (type)) ? F0_REGNUM : A1_REGNUM;
-  memcpy (valbuf, &regbuf[REGISTER_BYTE (regnum)], TYPE_LENGTH (type));
 }
 
 static unsigned int
