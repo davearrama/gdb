a4db09211b48249c039aaf0d43a273f93b76a383
ezannoni <ezannoni>
2002-11-03 22:07:42 +0000

    merge from mainline

--- gdb/testsuite/gdb.asm/asmsrc2.s
+++ gdb/testsuite/gdb.asm/asmsrc2.s
@@ -4,13 +4,15 @@
 comment "Second file in assembly source debugging testcase."
 
 	.global foo2
-foo2:
-	enter
+gdbasm_declare foo2
+	gdbasm_enter
 
-comment "Call someplace else."
+comment "Call someplace else (several times)."
 
-	call foo3
+	gdbasm_call foo3
+	gdbasm_call foo3
 
 comment "All done, return."
 
-	leave
+	gdbasm_leave
+	gdbasm_end foo2
